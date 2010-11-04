cb5b249c5325e503dcf32f4a910e14a26beaaa89
Felipe Kaufmann <felipekaufmann@gmail.com>
2010-11-04 01:04:44 +0100

    build menu from select options

--- jquery.dropSelect-0.0.2.js
+++ jquery.dropSelect-0.0.2.js
@@ -1,12 +1,31 @@
 /*
- *
- *
+ * TODO: title should be from selected option
+ * TODO: make stylable trough options 
+ * TODO: ability to add custom class to menu, button etc
  *
  */
 
 (function($) {
     $.fn.dropSelect = function(options) {
         var opts = $.extend({}, $.fn.dropSelect.defaults, options);
+        return this.each(function() {
+          $this = $(this);
+
+          button = $('<div class="dropselect-button">' + $this.find('option:selected').text() + '</div>').insertAfter(this);
+          dropdown = $('<ul class="dropselect-menu"></ul>').insertAfter(button);
+
+          $this.children().each(function() {
+              var option = $(this);
+              if (option.attr('selected') == true) {
+                  dropdown.append('<li class="selected">' + option.text() + '</li>');
+              } else {
+                  dropdown.append('<li>' + option.text() + '</li>');
+              }
+          });
+
+          // TODO: attach behaviour to button
+          // TODO: attach behaviour to menu links
+        });
     };
 
     $.fn.dropSelect.defaults = {
